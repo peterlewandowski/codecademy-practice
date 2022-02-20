e013c627a4acc30745b9b4098bd1b91a2db0ec9f
Peter Lewandowski <mapiotr2010@gmail.com>
2022-02-19 22:51:46 -0500

    first commit

---
+++ jsVariables.js
@@ -0,0 +1,20 @@
+const myName = 'Piotr'
+let myCity = 'Boca Raton'
+let myAge = 31
+const isMarried = false
+
+console.log(myName + myCity + myAge)
+console.log(myName + myCity + myAge + isMarried)
+
+// const thingOne // throws error
+let thingTwo
+var thingThree
+
+// console.log(thingOne)
+console.log(thingTwo)
+console.log(thingThree)
+
+console.log(typeof(myAge))
+console.log(typeof(myName))
+console.log(typeof(isMarried))
+
