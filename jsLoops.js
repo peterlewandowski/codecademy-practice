c3e7694199611845609ae4fb1d7ddfe42b55924e
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-04 01:21:21 -0500

    adding loops notes

--- jsLoops.js
+++ jsLoops.js
@@ -11,3 +11,56 @@ while (i < 5) {
     i++
 }
 
+// DO...WHILE
+
+x = 0
+i = 6
+
+do {
+    x = x + i;
+    console.log(x)
+    i++;
+} while (i < 5); // Prints: 0 1 3 6 10
+
+
+
+// FOR LOOP
+// initialization, stopping condition, iteration statement
+// initialize, stopper, iterate
+for (let i = 0; i < 4; i++) {
+    console.log(i);
+};
+// Output: 0, 1, 2, 3
+
+
+
+// LOOPING THROUGH ARRAYS
+
+/* for (let i = 0; i < Array.length; i++) {
+    console.log(array[i]);
+} */
+// Output: every item in the array
+
+
+
+// BREAK KEYWORD
+//initialize, stopper, iterate
+for (let i = 0; i < 99; i += 1) {
+    if (i > 5) {
+        break;
+    }
+    console.log(i)
+}
+// Output: 0 1 2 3 4 5 
+
+
+// NESTED FOR LOOP
+// init, stopper, iterate
+// inits, stopper, iterate
+
+for (let outer = 0; outer < 2; outer += 1) {
+    //init, stop, iterate
+    for (let inner = 0; inner < 3; inner += 1) {
+        console.log(`${outer}-${inner}`);
+    }
+}
